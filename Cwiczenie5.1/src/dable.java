b9c997e1cc5472320b9cfcad19a25e0fba5ef132
Daniel Orłowski <karixo717@gmail.com>
2012-08-29 22:02:42 +0200

    cwiczenie
    asdsadv

---
+++ Cwiczenie5.1/src/dable.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Daniel
+ * Date: 15.08.12
+ * Time: 13:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class dable {
+    public static void main(String[] args){
+        double zmienna1 = 14.5;
+        double zmienna2 = 24.45;
+        System.out.print(zmienna1);
+        System.out.print(zmienna2);
+    }
+}
