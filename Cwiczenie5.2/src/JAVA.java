21d5385786dec8ec15e87a22e2ec41f6051fc01f
Daniel Orłowski <karixo717@gmail.com>
2012-08-29 22:05:35 +0200

    cwiczenie2

---
+++ Cwiczenie5.2/src/JAVA.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Daniel
+ * Date: 15.08.12
+ * Time: 13:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class JAVA {
+      public static void main(String[] args){
+          System.out.println("|---  /---\\  |    | /---\\");
+          System.out.println("   |  |   |  |    | |   |");
+          System.out.println("   |  |---|  |    | |---|");
+          System.out.println("   |  |   |   \\  /  |   |");
+          System.out.println("\\__/  |   |    \\/   |   |");
+      }
+}
