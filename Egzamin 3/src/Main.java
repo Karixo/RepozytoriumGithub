22d52e912a728f4bed919269b8d5e191b69a81eb
Daniel Orłowski <karixo717@gmail.com>
2012-09-28 20:29:41 +0200

    egzamin3

---
+++ Egzamin 3/src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Daniel
+ * Date: 27.09.12
+ * Time: 20:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args){
+        int j=28;
+        for(int i=31; i<47; i++){
+            for(;;){
+                System.out.println();
+                if(j++ >=28){
+                    break;
+                }
+            }
+            System.out.print("97-"+i+"-98-"+i+"-99-"+i+"-100-"+i+"-101-"+i+"-102-"+i+"-"+j+"-");
+
+        }
+
+    }
+}
